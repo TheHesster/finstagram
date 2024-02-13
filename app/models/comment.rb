691924557e195a48dd4e12a9ed875fe3f46e382b
Adam Hess <adamhess89@gmail.com>
2024-02-13 03:12:45 +0000

    W6D1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
 class Comment < ActiveRecord::Base
     belongs_to :user
     belongs_to :finstagram_post
+
+    validates_presence_of :text, :user, :finstagram_post
+    
+    def humanized_time_ago
+        time_ago_in_seconds = Time.now - self.created_at
+        time_ago_in_minutes = time_ago_in_seconds / 60
+
+        if time_ago_in_minutes >= 60
+            "#{(time_ago_in_minutes / 60).to_i} hours ago"
+        else
+            "#{time_ago_in_minutes.to_i} minutes ago"
+        end
+    end
+
 end
